6346438f92a3fb454dafd41a6f01cb62d4856e8e
Matt Tang <mattang687@gmail.com>
2019-07-18 15:01:41 -0400

    implemented markdown conversion with Showdown

--- newTab/newTab.js
+++ newTab/newTab.js
@@ -2,3 +2,13 @@ var textarea = document.getElementById("newTabPad");
 textarea.onkeyup = save;
 
 getNotes();
+
+document.getElementById("convertButton").addEventListener("click", convert);
+
+function convert() {
+    var text = textarea.value,
+    markdownTarget = document.getElementById("markdownTarget"),
+    converter = new showdown.Converter(),
+    html = converter.makeHtml(text);
+    markdownTarget.innerHTML = html;
+}
