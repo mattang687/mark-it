e1d0898ee2d17bcf2185727767c72a689f47fb01
Matt Tang <mattang687@gmail.com>
2019-07-19 01:14:34 -0400

    tweaked appearance

--- newTab/newTab.js
+++ newTab/newTab.js
@@ -1,17 +1,22 @@
 var textarea = document.getElementById("newTabPad");
 var markdownTarget = document.getElementById("markdownTarget");
+const button = document.getElementById("changeViewButton");
 
 textarea.onkeyup = save;
 textarea.style.display = "none";
 
-getAndConvert();
+button.innerText = "VIEW";
+button.style.background = "#fb9f89";
+button.addEventListener('click', convertAndSwitch);
 
-browser.commands.onCommand.addListener(function(command) {
+browser.commands.onCommand.addListener(function (command) {
     if (command == "new_tab_switch_mode") {
-      console.log("switching mode");
+        console.log("switching mode");
         convertAndSwitch();
     }
-  });
+});
+
+getAndConvert();
 
 function convert() {
     var text = textarea.value;
@@ -28,8 +33,12 @@ function convertAndSwitch() {
     if (textarea.style.display === "none") {
         textarea.style.display = "block";
         markdownTarget.style.display = "none";
+        button.innerText = "EDIT";
+        button.style.background = "#81ae9d";
     } else {
         textarea.style.display = "none";
         markdownTarget.style.display = "block";
+        button.innerText = "VIEW";
+        button.style.background = "#fb9f89";
     }
 }
