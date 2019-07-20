969e327666a4b1c51f6a66b354aff7cf9d5a796d
Matt Tang <mattang687@gmail.com>
2019-07-20 18:04:21 -0400

    ported local storage to chrome

--- scripts/notes.js
+++ scripts/notes.js
@@ -4,7 +4,7 @@ function onError(error) {
 
 async function getNotes() {
     // we store notes in a single file, should only have one object in results
-    await browser.storage.local.get(null).then((results) => {
+    await chrome.storage.local.get(null).then((results) => {
         let text = results["notes"];
         if (text == null) {
             text = "Enter Notes Here!\n\n(Switch to Edit Mode with `Ctrl + Space` If You're in a New Tab)";
@@ -29,7 +29,7 @@ async function getAndConvert() {
 function save() {
     const key = "notes";
     const text = textarea.value;
-    const storingNote = browser.storage.local.set({ [key]: text });
+    const storingNote = chrome.storage.local.set({ [key]: text });
     storingNote.then(() => {
         console.log("saved");
     }, onError);
